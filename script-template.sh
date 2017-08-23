c4012e29b1e216cd47c849259b3a22d28e82042a
routingsparks <routingsparks@gmail.com>
2017-08-23 19:06:47 -0400

    Restructured directory.

---
+++ script-template.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#####################################################################
+# Script Title:
+# Script Description:
+# Author
+# Date Created
+# Version:  1.0 Initial script creation
+#           1.1 Modified line 7
+#           1.2 Added new directory variable.
+#####################################################################
