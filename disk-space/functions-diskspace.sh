1e4cb9639eafc022eaff23da02f4df44b981d59b
routingsparks <routingsparks@gmail.com>
2017-09-02 12:09:41 -0400

    added new scripts.

--- disk-space/functions-diskspace.sh
+++ disk-space/functions-diskspace.sh
@@ -5,12 +5,14 @@ HOMEDIRSPACE=$(df -h /home | tail -1 | awk '{print $5}' | tr -d "%")
 TMPDIRSPACE=$(df -h /tmp | tail -1 | awk '{print $5}' | tr -d "%")
 BOOTDIRSPACE=$(df -h /boot | tail -1 | awk '{print $5}' | tr -d "%")
 
+# Define functions
+function root_disk_space() {
 case $ROOTDIRSPACE in
 	100)
 		echo "The / partition is full."
 		echo "Emergency! Disk is full!" | mail -s "Disk emergency" root@localhost
         ;;
-  [0]*[1-9])
+  [0-9])
       echo "The / partion is currently under 10%."
     ;;
 	[1-7]*[0-9])
@@ -22,35 +24,39 @@ case $ROOTDIRSPACE in
 	*)
 		echo "Hmm, I expected to see some kind of percentage."
 esac
+}
 
+function home_disk_space() {
 case $HOMEDIRSPACE in
 	100)
 		echo "The /home partition is full."
 		echo "Emergency! Disk is full!" | mail -s "Disk emergency" root@localhost
-        ;;
-  [0-9])
-      echo "The /home partion is currently under 10%."
-    ;;
-	[1-7]*[0-9])
-		echo "The /home partition is currently $HOMEDIRSPACE% full."
-		;;
+      ;;
 	[8-9]*[0-9])
 		echo "Start clearing files. I am currently $HOMEDIRSPACE% full."
-		;;
+			;;
+	[1-7]*[0-9])
+		echo "The /home partition is currently $HOMEDIRSPACE% full."
+			;;
+  [0-9])
+    echo "The /home partion is currently $HOMEDIRSPACE% full."
+    	;;
 	*)
 		echo "Hmm, I expected to see some kind of percentage."
 esac
+}
 
+function boot_disk_space() {
 case $BOOTDIRSPACE in
 	100)
 		echo "The /boot partition is full."
 		echo "Emergency! Disk is full!" | mail -s "Disk emergency" root@localhost
         ;;
   [0-9])
-      echo "The /boot partion is currently under 10%."
+      echo "The /home partion is currently $BOOTDIRSPACE% full."
     ;;
 	[1-7]*[0-9])
-		echo "The /boot partition is currently $BOOTDIRSPACE% full."
+		echo "The /home partition is currently $BOOTDIRSPACE% full."
 		;;
 	[8-9]*[0-9])
 		echo "Start clearing files. I am currently $BOOTDIRSPACE% full."
@@ -58,3 +64,8 @@ case $BOOTDIRSPACE in
 	*)
 		echo "Hmm, I expected to see some kind of percentage."
 esac
+}
+
+root_disk_space
+home_disk_space
+boot_disk_space
---
+++ user-management/create-user.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#####################################################################
+# Script Title: create-user.sh
+# Script Description: Adds user with specifed home directory
+# Author:
+# Date Created: 2017-09-02
+# Version:  1.0 Initial script creation
+#           1.1 Modified line 8
+#####################################################################
