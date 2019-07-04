ebaad263296a0f891959e348052aed71144cdd32
routingsparks <routingsparks@gmail.com>
2019-07-04 19:28:35 -0400

    20190704

---
+++ scripts/sysadmin/cron-backup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#####################################################################
+# Script Title: Crontab Backup
+# Script Description: This script backs up the system and user cron jobs.
+# Author: Sparky
+# Date Created: 20190505
+# Version:  1.0 Initial script creation
+#####################################################################
+
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root"
+    crontab -l > /tmp/crontab-`date +%Y%m%d`.bak
+    fi
+
--- scripts/system/config-backup.sh
+++ scripts/system/config-backup.sh
@@ -3,9 +3,10 @@
 # Config file backup script
 #
 # Files being backed up:
+#   .atom
 #   .config/terminator
 #   .bash_profile
 #   .bashrc
 #   .vim
 
-tar -cJpf $HOME/backups/backup-config-`date +%Y%m%d`.tar.xz /home/sparky/{.config/terminator,.bash_profile,.bashrc,.vim}
+tar -cJpf $HOME/backups/backup-config-`date +%Y%m%d`.tar.xz /home/sparky/{.atom,.config/terminator,.bash_profile,.bashrc,.vim}
